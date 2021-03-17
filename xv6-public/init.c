9c0af267f35954844809d5c3885919a010709ac6
a00227347 <a00227347@itesm.mx>
2021-03-17 01:32:30 +0000

    Practica 2

--- xv6-public/init.c
+++ xv6-public/init.c
@@ -20,14 +20,16 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+    printf(1, "galletitas sponge");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
       exit();
     }
     if(pid == 0){
-      exec("sh", argv);
+		// 1) cuando cambié ls se quedó en un for infinito	
+		// 2) noexisteprograma falla y vuelve a iniciar
+      exec("noexisteprograma", argv);
       printf(1, "init: exec sh failed\n");
       exit();
     }
